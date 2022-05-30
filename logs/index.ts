160a93c463a334452a197bca15b4db51da3f64db
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-05-29 20:16:09 -0600

    feat: add listify func

---
+++ logs/index.ts
@@ -0,0 +1,29 @@
+type ListFormatOptions = {
+  type?: "conjunction" | "disjunction" | "unit";
+  style?: "long" | "short" | "narrow";
+  localeMatcher?: "lookup" | "best fit";
+};
+declare namespace Intl {
+  class ListFormat {
+    constructor(locale: string, options: ListFormatOptions);
+    public format: (items: Array<string>) => string;
+  }
+}
+
+type ListifyOptions<ItemType> = {
+  type?: ListFormatOptions["type"];
+  style?: ListFormatOptions["style"];
+  stringify?: (item: ItemType) => string;
+};
+function listify<ItemType>(
+  array: Array<ItemType>,
+  {
+    type = "conjunction",
+    style = "long",
+    stringify = (thing: { toString(): string }) => thing.toString(),
+  }: ListifyOptions<ItemType> = {}
+) {
+  const stringified = array.map((item) => stringify(item));
+  const formatter = new Intl.ListFormat("en", { style, type });
+  return formatter.format(stringified);
+}
